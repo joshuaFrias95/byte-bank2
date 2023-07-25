c56047a23f7f7501de7cb282a1bc799de875644a
joshuaFrias95 <metatron1495@gmail.com>
2023-07-25 02:03:18 -0600

    Add: Concluyo curso java.utils

--- src/com/bytebank/modelo/GuardaCuentas.java
+++ src/com/bytebank/modelo/GuardaCuentas.java
@@ -1,24 +1,20 @@
 package com.bytebank.modelo;
 
+/**
+ * Clase que nos servirá para guardar cuentas
+ * Permite agregar cuentas con un método
+ */
 public class GuardaCuentas {
-	
-	// Crear un objeto para guardar muchas Cuentas
-	// Permitirnos agregar cuentas con un metodo 
-	// guardaCuentas.adiciona(cuenta);
-	// obtener, remover, etc.
-	
+
 	Cuenta[] cuenta = new Cuenta[10];
-	int indice = 0;
+	int index = 0;
 
-	
-	//                    [X  | X | X | X | ... |  ]
 	public void adicionar(Cuenta cc) {
-		cuenta[indice] = cc;
-		indice++;
-	}
-	
-	public Cuenta obtener(int indice) {
-		return cuenta[indice];
+		cuenta[index] = cc;
+		index++;
 	}
 
+	public Cuenta obtener(int index) {
+		return cuenta[index];
+	}
 }
--- src/com/bytebank/modelo/GuardaReferencias.java
+++ src/com/bytebank/modelo/GuardaReferencias.java
@@ -1,22 +1,21 @@
 package com.bytebank.modelo;
 
+/**
+ * Objeto que nos permite guardar muchas cuentas
+ * Permite agregar cuentas con un método
+ *
+ */
 public class GuardaReferencias {
-	
-	// Crear un objeto para guardar muchas Cuentas
-	// Permitirnos agregar cuentas con un metodo 
-	// guardaCuentas.adiciona(cuenta);
-	// obtener, remover, etc.
-	
+
 	Object[] referencia = new Object[10];
 	int indice = 0;
 
-	
-	//                    [X  | X | X | X | ... |  ]
+
 	public void adicionar(Object cc) {
 		referencia[indice] = cc;
 		indice++;
 	}
-	
+
 	public Object obtener(int indice) {
 		return referencia[indice];
 	}
